f50821a219de3aff600c7120c67901b0ec1900f5
Alex Thomas <amthomas@vt.edu>
2011-06-17 23:06:19 -0400

    Revamped the class system and added a first scene. More to come this weekend!
    Change-Id: I471f14fdd6b807a3db8be8e58c56882b25d012ae

--- src/Game.java
+++ src/Game.java
@@ -1,53 +1,8 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-
-public class Game implements ActionListener {
-
-	JTextArea text;
-	private int levelNum;
-	private String levelText;
-		
-	// creates a new game
-	public static void main (String[] args) {
-		Game gui = new Game();
-		
-		
-		
-		
-		int levelNum = 1;
-		String levelText = "test";
-		Level stage = new Level(levelNum, levelText);
-		stage.refresh();
-		
+public class Game{
+	
+	public static void main (String[] args){
+		GameGUI gui = new GameGUI();
 		gui.initialize();
-		
-	}
-	
-	public void initialize() {
-		// initializes the window
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		JButton button = new JButton("Okay");
-		button.addActionListener(this);
-		text = new JTextArea(20,30);
-		text.setLineWrap(true);
-		text.setEditable(false);
-		JScrollPane scroller = new JScrollPane(text);
-		scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		panel.add(scroller);
-		frame.getContentPane().add(BorderLayout.CENTER, panel);
-		frame.getContentPane().add(BorderLayout.SOUTH, button);
-		frame.setSize(380,400);
-		frame.setVisible(true);
-		
-		text.append(levelText + levelNum + " \n");
-		
-	}
-	
-	public void actionPerformed(ActionEvent ev) {
-		text.append("button clicked \n");
 	}
 	
 }
--- src/Level.java
+++ src/Level.java
@@ -1,42 +1,83 @@
-public class Level extends Game {
+public class Level {
 	private int levelNum;
 	private String levelText;
+	private String buttonLabel;
 
-	public Level (int levelNum, String levelText){
+	public Level (int levelNum, String levelText, String buttonLabel) {
 		this.levelNum = levelNum;
 		this.levelText = levelText;
-		
-		// stuff to edit variables
+		this.buttonLabel = buttonLabel;
 	}
-	
 
+	public void levelCheck() {
+		if (getLevelNum() == 1) {
+			setLevelText("I've been waiting here for quite a while." + "\n");
+			setButtonLabel("Continue");
+		}
+		if (getLevelNum() == 2) {
+			setLevelText("I look up at the dark, slow-moving coulds and become" + "\n" +
+					"restless." + "\n");
+			setButtonLabel("Continue");
+		}
+		if (getLevelNum() == 3) {
+			setLevelText("It must be getting late. I wonder what time it is." + "\n");
+			setButtonLabel("Check my watch");
+		}
+		if (getLevelNum() == 4) {
+			setLevelText("3:01am. He was supposed to be here an hour ago. I" + "\n" +
+					"guess I could wait longer." + "\n");
+			setButtonLabel("Continue");
+		}
+		if (getLevelNum() == 4) {
+			setLevelText("It's not like I have anything better to do. ");
+			setButtonLabel("Continue");
+		}
+		if (getLevelNum() == 5) {
+			setLevelText("It begins to pour." + "\n");
+			setButtonLabel("Continue");
+		}
+		if (getLevelNum() == 6) {
+			setLevelText("The thunderstorm is getting worse. He was probably" + "\n" +
+					"apprehended by the Night Watch." + "\n");
+			setButtonLabel("Check my watch");
+		}
+		if (getLevelNum() == 7) {
+			setLevelText("3:32am. Well, that settles it. This was a foolish" + "\n" +
+					"ploy anyway. Who in their right mind would have gone" + "\n" +
+					"through with this?!" + "\n");
+			setButtonLabel("Continue");
+		}
+		if (getLevelNum() == 8) {
+			setLevelText("I've risked enough. Time to get out of here." + "\n");
+			setButtonLabel("Get on my motorcycle" + "\n");
+		}
+		
+		int levelNum = getLevelNum() + 1;
+		setLevelNum(levelNum);
+	}
 
 	public int getLevelNum() {
 		return levelNum;
 	}
-
-
-
+	
 	public void setLevelNum(int levelNum) {
 		this.levelNum = levelNum;
 	}
-
-
-
+	
 	public String getLevelText() {
 		return levelText;
 	}
-
-
-
+	
 	public void setLevelText(String levelText) {
 		this.levelText = levelText;
 	}
+	
+	public String getButtonLabel() {
+		return buttonLabel;
+	}
 
-
-
-	public void refresh() {
-		System.out.println(levelText + levelNum);
+	public void setButtonLabel(String buttonLabel) {
+		this.buttonLabel = buttonLabel;
 	}
 
 }
