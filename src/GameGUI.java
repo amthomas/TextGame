acd9ad62fbf34144e788896823525423a69bd8ec
Alex Thomas <amthomas@vt.edu>
2011-06-20 13:14:25 -0400

    Strange problem.
    Change-Id: I586c2fd3e7dfb63ae33bb6616aabae82bf11b354

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -4,7 +4,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
@@ -13,11 +15,14 @@ public class GameGUI implements ActionListener{
 
 	JTextArea text;
 	JButton button;
+	JPasswordField passwordField;
+	JLabel label;
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	
 	Level stage = new Level(1, "", "");
 		
 	public void initialize(){
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
 		text = new JTextArea(20,30);
 		text.setLineWrap(true);
 		text.setEditable(false);
@@ -39,6 +44,39 @@ public class GameGUI implements ActionListener{
 	
 	public void actionPerformed(ActionEvent ev) {
 		stage.levelCheck();
+		
+		if (stage.getLevelNum() == 15) {
+//			text = new JTextArea(20,30);
+//			text.setLineWrap(true);
+//			text.setEditable(false);
+//			JScrollPane scroller = new JScrollPane(text);
+//			scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+//			scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+//			panel.add(scroller);
+//			
+//			
+//			
+//			frame.setTitle("The Wanderer");
+//			frame.getContentPane().add(BorderLayout.CENTER, panel);
+//			frame.getContentPane().add(BorderLayout.SOUTH, button);
+//			frame.setSize(380,400);
+//			frame.setResizable(false);
+//			frame.setVisible(true);			
+			
+			
+			String okay = "ok";
+			frame.getContentPane().remove(button);
+			passwordField = new JPasswordField(4);
+			passwordField.setActionCommand("Okay");
+			passwordField.addActionListener(this);
+			label = new JLabel("Enter the password: ");
+			label.setLabelFor(passwordField);
+			
+			panel.add(passwordField);
+		}
+		
+		
+		
 		refresh();
 	}
 	
@@ -46,7 +84,18 @@ public class GameGUI implements ActionListener{
 	public void refresh() {
 		String buttonLabel = stage.getButtonLabel();
 		button.setLabel(buttonLabel);
+		
+
 		text.append(stage.getLevelText());
 	}
+	
+//	public void actionPerformed(ActionEvent e) {
+//		String cmd = "e.getActionCommand();"
+//		
+//			if (OK.equals(cmd)) { //process the password
+//				char[] input = passwordField.getpassword();
+//				
+//			}
+//	}
 	
 }
