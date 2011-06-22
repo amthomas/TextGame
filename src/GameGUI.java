6e89ecfa5f72977302d86ab01a5d4410520bc43f
Alex Thomas <amthomas@vt.edu>
2011-06-21 22:27:55 -0400

    Added a password to the "first" level. Then looped it back around to a generic "Continue" button. It took a long time!
    Change-Id: Id9275ea9ffbbd9d7d5993f2bd8ee5243d4a6712d

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
@@ -42,63 +43,64 @@ public class GameGUI implements ActionListener{
 		frame.setVisible(true);
 	}
 	
+	// password listener class... why does it still want me to implement ActionListener?
+	class AL implements ActionListener{ 
+		public void actionPerformed(ActionEvent e) {
+			if (stage.getLevelNum() == 15) {
+				String password = "2965";
+				JPasswordField input = (JPasswordField) e.getSource();
+				char[] passyArray = input.getPassword();
+				String passy = new String(passyArray);
+			
+				if (passy.equals(password)){
+					JOptionPane.showMessageDialog(null, "Computer: Access granted");
+					frame.getContentPane().remove(passwordField);
+					frame.getContentPane().add(BorderLayout.SOUTH, button);
+					stage.levelCheck();
+					refresh();
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Computer: Access denied");
+				}
+			}
+		}
+	}
+
 	public void actionPerformed(ActionEvent ev) {
-		
-		stage.levelCheck();
-		
+		if (stage.getLevelNum() < 15) {
+			stage.levelCheck();
+			refresh();
+		}
 		if (stage.getLevelNum() == 15) {
-//			text = new JTextArea(20,30);
-//			text.setLineWrap(true);
-//			text.setEditable(false);
-//			JScrollPane scroller = new JScrollPane(text);
-//			scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-//			scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-//			panel.add(scroller);
-//			
-//			
-//			
-//			frame.setTitle("The Wanderer");
-//			frame.getContentPane().add(BorderLayout.CENTER, panel);
-//			frame.getContentPane().add(BorderLayout.SOUTH, button);
-//			frame.setSize(380,400);
-//			frame.setResizable(false);
-//			frame.setVisible(true);			
-			
-			
-			String okay = "ok";
 			frame.getContentPane().remove(button);
 			passwordField = new JPasswordField(4);
-			passwordField.setActionCommand("Okay");
-			passwordField.addActionListener(this);
-			label = new JLabel("Enter the password: ");
-			label.setLabelFor(passwordField);
-			
-			panel.add(passwordField);
-			
-			
+			frame.getContentPane().add(BorderLayout.SOUTH, passwordField);
+			passwordField.addActionListener(new AL());
 		}
-		
-		
-		
-		refresh();
+		if (stage.getLevelNum() > 15) {
+			
+//			button = new JButton("Start");
+//			button.addActionListener(this);
+			
+			stage.levelCheck();
+			refresh();
+		}
 	}
+		
 	
-//	@SuppressWarnings("deprecation")
+	
+	@SuppressWarnings("deprecation")
 	public void refresh() {
 		String buttonLabel = stage.getButtonLabel();
 		button.setLabel(buttonLabel);
-		
-
 		text.append(stage.getLevelText());
-	}
+	}		
+
 	
+
+
 //	public void actionPerformed(ActionEvent e) {
-//		String cmd = "e.getActionCommand();"
-//		
-//			if (OK.equals(cmd)) { //process the password
-//				char[] input = passwordField.getpassword();
-//				
-//			}
+
 //	}
 	
 }
--- src/Level.java
+++ src/Level.java
@@ -1,9 +1,67 @@
 public class Level {
-	private static final String LEVEL_1_TEXT = "I've been waiting here for quite a while.";
 	private int levelNum;
 	private String levelText;
 	private String buttonLabel;
+	
+	private static final String LEVEL_1_TEXT_A = "I've been waiting here for quite a while.";
+	private static final String LEVEL_1_BUTTON = "Continue";
+	
+	private static final String LEVEL_2_TEXT_A = "I look up at the dark, slow-moving coulds and become";
+	private static final String LEVEL_2_TEXT_B = "restless.";
+	private static final String LEVEL_2_BUTTON = "Continue";
+	
+	private static final String LEVEL_3_TEXT_A = "It must be getting late. I wonder what time it is.";
+	private static final String LEVEL_3_BUTTON = "Check my watch";
+	
+	private static final String LEVEL_4_TEXT_A = "3:01am. He was supposed to be here an hour ago. I";
+	private static final String LEVEL_4_TEXT_B = "guess I could wait longer.";
+	private static final String LEVEL_4_BUTTON = "Continue";
+	
+	private static final String LEVEL_5_TEXT_A = "It's not like I have anything better to do. ";
+	private static final String LEVEL_5_BUTTON = "Continue";
+	
+	private static final String LEVEL_6_TEXT_A = "It begins to pour.";
+	private static final String LEVEL_6_BUTTON = "Continue";
+	
+	private static final String LEVEL_7_TEXT_A = "The thunderstorm is getting worse. He was probably";
+	private static final String LEVEL_7_TEXT_B = "apprehended by the Night Watch.";
+	private static final String LEVEL_7_BUTTON = "Check my watch";
 
+	private static final String LEVEL_8_TEXT_A = "3:32am. Well, that settles it. This was a foolish";
+	private static final String LEVEL_8_TEXT_B = "ploy anyway. Who in their right mind would have gone";
+	private static final String LEVEL_8_TEXT_C = "through with this?!";
+	private static final String LEVEL_8_BUTTON = "Continue";
+
+	private static final String LEVEL_9_TEXT_A = "I've risked enough. Time to get out of here.";
+	private static final String LEVEL_9_BUTTON = "Get on my motorcycle";
+	
+	private static final String LEVEL_10_TEXT_A = "As I get on my motorcycle, a dark figure approaches.";
+	private static final String LEVEL_10_BUTTON = "Continue";
+	
+	private static final String LEVEL_11_TEXT_A = "David: Going somewhere?";
+	private static final String LEVEL_11_BUTTON = "Continue";
+	
+	private static final String LEVEL_12_TEXT_A = "Capa: I thought you didn't make it.";
+	private static final String LEVEL_12_BUTTON = "Continue";
+	
+	private static final String LEVEL_13_TEXT_A = "David: Well, here's the data disk. You should concentrate on";
+	private static final String LEVEL_13_TEXT_B = "GUI implementation instead of story development.";
+	private static final String LEVEL_13_BUTTON = "Continue";
+
+	private static final String LEVEL_14_TEXT_A = "Capa: Sounds good." + "\n";
+	private static final String LEVEL_14_BUTTON = "Drive away";
+	
+	private static final String LEVEL_15_TEXT_A = "You find yourself on the side of a skyscraper looking at";
+	private static final String LEVEL_15_TEXT_B = "the security keypad to a sealed door. You put on your";
+	private static final String LEVEL_15_TEXT_C = "infrared goggles. The numbers 5, 6, 9, and 2 are";
+	private static final String LEVEL_15_TEXT_D = "ordered from most hot to least hot. A security guard was";
+	private static final String LEVEL_15_TEXT_E = "here just one minute ago. What is access code?";
+	private static final String LEVEL_15_BUTTON = "Access code:";
+	
+	private static final String LEVEL_16_TEXT_A = "The giant sealed door silently slides open. After you";
+	private static final String LEVEL_16_TEXT_B = "slip inside, it slowly seals shut behind you.";
+	private static final String LEVEL_16_BUTTON = "Continue";
+		
 	public Level (int levelNum, String levelText, String buttonLabel) {
 		this.levelNum = levelNum;
 		this.levelText = levelText;
@@ -11,80 +69,69 @@ public class Level {
 	}
 
 	public void levelCheck() {
-		
-		switch (getLevelNum()){
-			case 1: setLevelText(LEVEL_1_TEXT + "\n");
-					setButtonLabel("Continue");
-		}
-//		if (getLevelNum() == 1) {
-//			setLevelText("I've been waiting here for quite a while." + "\n");
-//			setButtonLabel("Continue");
-//		}
-		if (getLevelNum() == 2) {
-			setLevelText("I look up at the dark, slow-moving coulds and become" + "\n" +
-					"restless." + "\n");
-		}
-		if (getLevelNum() == 3) {
-			setLevelText("It must be getting late. I wonder what time it is." + "\n");
-			setButtonLabel("Check my watch");
-		}
-		if (getLevelNum() == 4) {
-			setLevelText("3:01am. He was supposed to be here an hour ago. I" + "\n" +
-					"guess I could wait longer." + "\n");
-			setButtonLabel("Continue");
-		}
-		if (getLevelNum() == 4) {
-			setLevelText("It's not like I have anything better to do. ");
-		}
-		if (getLevelNum() == 5) {
-			setLevelText("It begins to pour." + "\n");
-		}
-		if (getLevelNum() == 6) {
-			setLevelText("The thunderstorm is getting worse. He was probably" + "\n" +
-					"apprehended by the Night Watch." + "\n");
-			setButtonLabel("Check my watch");
-		}
-		if (getLevelNum() == 7) {
-			setLevelText("3:32am. Well, that settles it. This was a foolish" + "\n" +
-					"ploy anyway. Who in their right mind would have gone" + "\n" +
-					"through with this?!" + "\n");
-			setButtonLabel("Continue");
-		}
-		if (getLevelNum() == 8) {
-			setLevelText("I've risked enough. Time to get out of here." + "\n");
-			setButtonLabel("Get on my motorcycle");
-		}
-		
-		if (getLevelNum() == 9) {
-			setLevelText("As I get on my motorcycle, a dark figure approaches." + "\n");
-			setButtonLabel("Continue");
-		}
-		if (getLevelNum() == 10) {
-			setLevelText("David: Going somewhere?" + "\n");
-			setButtonLabel("Continue");
-		}
-		if (getLevelNum() == 11) {
-			setLevelText("Capa: I thought you didn't make it." + "\n");
-			setButtonLabel("Continue");
-		}
-		if (getLevelNum() == 12) {
-			setLevelText("David: Well here's the data disk. You should concentrate on" + "\n" +
-					"GUI implementation instead of story development." + "\n");
-			setButtonLabel("Continue");
-		}
-		if (getLevelNum() == 13) {
-			setLevelText("Capa: Sounds good." + "\n");
-			setButtonLabel("Drive away");
-		}
-		if (getLevelNum() == 14) {
-			setLevelText("\n" + "You find yourself on the side of a skyscraper looking at"+ "\n" +
-					"the security keypad to a sealed door. You put on your infrared goggles." + "\n" +
-					"The numbers 5, 6, 9, and 2 are ordered from most hot to least hot. A" + "\n" +
-					"security guard was here just one minute ago. What is the password?");
-			setButtonLabel("Drive away");
-		}
 		int levelNum = getLevelNum() + 1;
 		setLevelNum(levelNum);
+		switch (getLevelNum()){
+			case 1: setLevelText(LEVEL_1_TEXT_A + "\n\n");
+					setButtonLabel(LEVEL_1_BUTTON);
+					break;
+			case 2: setLevelText(LEVEL_2_TEXT_A + "\n" +
+					LEVEL_2_TEXT_B + "\n\n");
+					setButtonLabel(LEVEL_2_BUTTON);
+					break;
+			case 3: setLevelText(LEVEL_3_TEXT_A + "\n\n");
+					setButtonLabel(LEVEL_3_BUTTON);
+					break;
+			case 4: setLevelText(LEVEL_4_TEXT_A + "\n" +
+					LEVEL_4_TEXT_B + "\n\n");
+					setButtonLabel(LEVEL_4_BUTTON);
+					break;
+			case 5: setLevelText(LEVEL_5_TEXT_A);
+					setButtonLabel(LEVEL_5_BUTTON);
+					break;
+			case 6: setLevelText(LEVEL_6_TEXT_A + "\n\n");
+					setButtonLabel(LEVEL_6_BUTTON);
+					break;
+			case 7: setLevelText(LEVEL_7_TEXT_A + "\n" +
+					LEVEL_7_TEXT_B + "\n\n");
+					setButtonLabel(LEVEL_7_BUTTON);
+					break;
+			case 8: setLevelText(LEVEL_8_TEXT_A + "\n" +
+					LEVEL_8_TEXT_B + "\n" +
+					LEVEL_8_TEXT_C + "\n\n");
+					setButtonLabel(LEVEL_8_BUTTON);
+					break;
+			case 9: setLevelText(LEVEL_9_TEXT_A + "\n\n");
+					setButtonLabel(LEVEL_9_BUTTON);
+					break;
+			case 10: setLevelText(LEVEL_10_TEXT_A + "\n\n");
+					setButtonLabel(LEVEL_10_BUTTON);
+					break;
+			case 11: setLevelText(LEVEL_11_TEXT_A + "\n\n");
+					setButtonLabel(LEVEL_11_BUTTON);
+					break;
+			case 12: setLevelText(LEVEL_12_TEXT_A + "\n\n");
+					setButtonLabel(LEVEL_12_BUTTON);
+					break;
+			case 13: setLevelText(LEVEL_13_TEXT_A + "\n" +
+					LEVEL_13_TEXT_B + "\n\n");
+					setButtonLabel(LEVEL_13_BUTTON);
+					break;
+			case 14: setLevelText(LEVEL_14_TEXT_A + "\n\n");
+					setButtonLabel(LEVEL_14_BUTTON);
+					break;
+			case 15: setLevelText(LEVEL_15_TEXT_A + "\n" +
+					LEVEL_15_TEXT_B + "\n" +
+					LEVEL_15_TEXT_C + "\n" +
+					LEVEL_15_TEXT_D + "\n" +
+					LEVEL_15_TEXT_E + "\n\n");
+					setButtonLabel(LEVEL_15_BUTTON);
+					break;
+			case 16: setLevelText(LEVEL_16_TEXT_A + "\n" +
+					LEVEL_16_TEXT_B + "\n\n");
+					setButtonLabel(LEVEL_16_BUTTON);
+					break;
+		}
 	}
 
 	public int getLevelNum() {
